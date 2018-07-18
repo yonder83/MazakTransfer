6c4adada1ff8d3aa44fe248ebbf6aaca0ba73e84
tuomo <tuomo@DESKTOP-D02E0SO>
2018-07-19 02:43:23 +0300

    -Added file search to its own thread

--- MazakTransfer/Util/FileSearcher.cs
+++ MazakTransfer/Util/FileSearcher.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Threading;
 
 namespace MazakTransfer.Util
@@ -7,7 +10,6 @@ namespace MazakTransfer.Util
     public class FileSearcher
     {
         private readonly Dispatcher _dispatcher;
-        private Action<IEnumerable<FileData>> _doneAction;
         private bool _searching;
 
         public FileSearcher(Dispatcher dispatcher)
@@ -24,21 +26,25 @@ namespace MazakTransfer.Util
                 if (!_searching)
                 {
                     _searching = true;
-                    _doneAction = actionToCallWhenDone;
-                    Func<string, string, IEnumerable<FileData>> methodToCallAsync = DirectoryManager.GetFilesInfo;
-                    methodToCallAsync.BeginInvoke(filePath, searchPattern, SearchDone, methodToCallAsync);
+
+                    var tokenSource = new CancellationTokenSource();
+
+                    var task = Task.Factory.StartNew(SearchBackground(filePath, searchPattern), tokenSource.Token);
+                    var continuationTask = task.ContinueWith(t =>
+                    {
+                        _searching = false;
+
+                        //Kutsutaan paluumetodia, palautetaan sille tiedostolista
+                        _dispatcher.Invoke(actionToCallWhenDone, t.Result);
+                    }, tokenSource.Token);
                 }
             }
         }
 
-        private void SearchDone(IAsyncResult ar)
+        private static Func<List<FileData>> SearchBackground(string filePath, string searchPattern)
         {
-            _searching = false;
-            var target = (Func<string, string, IEnumerable<FileData>>) ar.AsyncState;
-            IEnumerable<FileData> result = target.EndInvoke(ar);
-
-            //Kutsutaan paluumetodia, palautetaan sille tiedostolista
-            _dispatcher.Invoke(_doneAction, result);
+            //Create file searcher thread. GetFilesInfo returns with yield, so we must  make List here to get all files at once to continuation thread
+            return () => DirectoryManager.GetFilesInfo(filePath, searchPattern).ToList();
         }
     }
 }
