70624e88944f3f27b742afaa19a1b016fd51e07f
tuomo <tuomo@DESKTOP-D02E0SO>
2018-07-20 00:15:35 +0300

    -Moved EF configuration from App.config to code

--- MazakTransfer/Database/DbInitializer.cs
+++ MazakTransfer/Database/DbInitializer.cs
@@ -1,16 +1,26 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MazakTransfer.Database
 {
+    public class EF6CodeConfig : DbConfiguration
+    {
+        public EF6CodeConfig()
+        {
+            //Set SqlCe provider for EF6
+            SetProviderServices("System.Data.SqlServerCe.4.0", System.Data.Entity.SqlServerCompact.SqlCeProviderServices.Instance);
+
+            //Disable code first migrations. (Stops queries to __MigrationHistory table)
+            SetDatabaseInitializer<MazakTransferContext>(null);
+        }
+    }
+
     internal static class DbInitializer
     {
         public static void Initialize()
         {
-            //Disable code first migrations. (Stops queries to __MigrationHistory table)
-            System.Data.Entity.Database.SetInitializer<MazakTransferContext>(null);
-            
-            //Make query to database. This is first query with EF and takes longer because EF initializes itself
+            //Make query to database in background. This is first query with EF and takes longer because EF initializes itself
             Task.Factory.StartNew(() =>
             {
                 using (var context = new MazakTransferContext())
