f2d2f3e2585c9e46d61c00491b6f5466d3918ffe
tuomo <tuomo@DESKTOP-D02E0SO>
2019-05-20 18:30:36 +0300

    Removed logger

--- MazakTransfer/Util/Logger.cs
+++ MazakTransfer/Util/Logger.cs
@@ -1,15 +1,15 @@
-﻿using log4net;
-using log4net.Config;
+﻿//using log4net;
+//using log4net.Config;
 
-namespace MazakTransfer.Util
-{
-    public static class Logger
-    {
-        static Logger()
-        {
-            XmlConfigurator.Configure();
-        }
+//namespace MazakTransfer.Util
+//{
+//    public static class Logger
+//    {
+//        static Logger()
+//        {
+//            XmlConfigurator.Configure();
+//        }
 
-        public static ILog ProgramLogger => LogManager.GetLogger("ProgramLogger");
-    }
-}
+//        public static ILog ProgramLogger => LogManager.GetLogger("ProgramLogger");
+//    }
+//}
