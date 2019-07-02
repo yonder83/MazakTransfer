3971d51f983ae41c1774b47253eefba9ed9bd671
tuomo <tuomo.kuusela@hotmail.com>
2019-07-02 14:42:46 +0300

    -Settings dialog development

--- MazakTransfer/SettingsWindow.xaml.cs
+++ MazakTransfer/SettingsWindow.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Forms;
 
 namespace MazakTransfer
 {
@@ -17,10 +7,51 @@ namespace MazakTransfer
     /// Interaction logic for SettingsWindow.xaml
     /// </summary>
     public partial class SettingsWindow : Window
-    {
+    { 
         public SettingsWindow()
         {
             InitializeComponent();
+
+            TextBoxLocalPath.Text = Properties.Settings.Default.LocalPath;
+            TextBoxMazakPath.Text = Properties.Settings.Default.MazakPath;
+            TextBoxUpdateInterval.Text = Properties.Settings.Default.FileListUpdateInterval.ToString();
+        }
+
+        private void ButtonLocalPath_Click(object sender, RoutedEventArgs e)
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (!string.IsNullOrEmpty(Properties.Settings.Default.LocalPath))
+                {
+                    folderBrowserDialog.SelectedPath = Properties.Settings.Default.LocalPath;
+                }
+
+                folderBrowserDialog.ShowDialog();
+                TextBoxLocalPath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void ButtonMazakPath_Click(object sender, RoutedEventArgs e)
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (!string.IsNullOrEmpty(Properties.Settings.Default.MazakPath))
+                {
+                    folderBrowserDialog.SelectedPath = Properties.Settings.Default.MazakPath;
+                }
+
+                folderBrowserDialog.ShowDialog();
+                TextBoxMazakPath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: save settings to custom settings file
+            //Properties.Settings.Default.LocalPath = TextBoxLocalPath.Text;
+            //Properties.Settings.Default.MazakPath = TextBoxMazakPath.Text;
+            //Properties.Settings.Default.Save();
+            Close();
         }
     }
 }
